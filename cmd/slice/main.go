af66b63fa9dd174ca7312dd54e5b0ab41e5eadc0
Mike Helmick <helmick@google.com>
2020-01-02 15:20:36 -0800

    add 2D slice example

--- cmd/slice/main.go
+++ cmd/slice/main.go
@@ -29,4 +29,20 @@ func main() {
 	// Show array and slice types are distinct
 	// arr := [2]int{1, 2}
 	// printAll(arr)
+
+	{
+		// Example of 2D slices
+		board := make([][]int, 5)
+		for i := 0; i < len(board); i++ {
+			board[i] = make([]int, 5)
+		}
+		i := 0
+		for y := 0; y < len(board); y++ {
+			for x := 0; x < len(board[y]); x++ {
+				board[y][x] = i
+				i++
+			}
+		}
+		log.Printf("Board: %v", board)
+	}
 }
